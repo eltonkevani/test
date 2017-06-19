75898fe06cc915ca27e080229d3e330735054a2c
eltonkevani <qevani.elton@gmail.com>
2017-06-19 15:20:22 +0200

    Update template.groovy

--- template.groovy
+++ template.groovy
@@ -1,8 +1,11 @@
+def rel = getCurrentRelease()
+def variable1 = rel.variables['variable1'].value?
+
 xlr {
   release('test') {
     variables {
       stringVariable('variable1') {
-        value 'lalala'
+        value variable1
       }
     }
     scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2017-05-08T09:00:00+0200')
