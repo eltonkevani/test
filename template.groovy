42bb28c8e7400373972d5a6e12465f0a78ed6e3c
eltonkevani <qevani.elton@gmail.com>
2017-06-19 15:18:29 +0200

    Update template.groovy

--- template.groovy
+++ template.groovy
@@ -1,29 +1,16 @@
 xlr {
-  release('testTemplate ${XLD_BRANCH}') {
-    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2017-05-04T09:00:00+0200')
-    scriptUsername 'admin'
-    scriptUserPassword '{b64}YFKOzMTEICsqFJ592l2FbQ=='
-    phases {
-      phase('Phase 1') {
-        color '#009CDB'
-        tasks {
-          manual('bla') {
-            
-          }
-        }
+  release('test') {
+    variables {
+      stringVariable('variable1') {
+        value 'lalala'
       }
+    }
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2017-05-08T09:00:00+0200')
+    phases {
       phase('New Phase') {
         tasks {
-          custom('script') {
-            script {
-              type 'remoteScript.Unix'
-              script 'echo lalala'
-              remotePath '/home/elton/tmp'
-              temporaryDirectoryPath '/home/elton/tmp'
-              address '192.168.0.50'
-              username 'elton'
-              password '{b64}OfgIffERzGxh1qLyySz73Q=='
-            }
+          manual('manual ${variable1}') {
+            
           }
         }
       }
